83b1946111da68bed26580655d929e259e1aa83d
Suprakit Sujariyakul <suprakitsujariyakul@gmail.com>
2023-02-16 23:36:28 +0700

    create UI on Login Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<form class="flex flex-col m-auto text-center p-10  bg-green-100 shadow-2xl rounded-3xl">
+	<label for="username" class="mb-2 text-xl">Username</label>
+	<input
+		type="text"
+		id="username"
+		class="border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-5"
+		bind:value={username}
+		required
+	/>
+	<label for="password" class="mb-2 text-xl">Password</label>
+	<input
+		type="text"
+		id="password"
+		class="border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-5"
+		bind:value={password}
+		required
+	/>
+	<button
+		type="submit"
+		class="text-white bg-red-200 hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-red-100  text-xl rounded-lg py-2.5 mt-2 text-center"
+		on:click={onLogin}>Login</button
+	>
+</form>
